564d1327bb37978e1bae5ad3b702f4ea921537cd
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-08-18 16:29:07 -0300

    add realTimeDataBase credent

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,22 +1,36 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getStorage, ref } from "firebase/storage";
+import { getDataBase, ref, set } from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBv5p7CSXCwBkq2I6DCjSy0nZrDKgUPsj0",
-  authDomain: "preparetec-api.firebaseapp.com",
-  projectId: "preparetec-api",
-  storageBucket: "preparetec-api.appspot.com",
-  messagingSenderId: "676397684205",
-  appId: "1:676397684205:web:2ba34fff6637084797c3e4",
-  measurementId: "G-0D04Y6850X"
+  apiKey: "AIzaSyAd_om9e0BidLaWtfOSx31knonTz0l5tCo",
+  authDomain: "preparetecapi-5d1fc.firebaseapp.com",
+  databaseURL: "https://preparetecapi-default-rtdb.firebaseio.com",
+  projectId: "preparetecapi",
+  storageBucket: "preparetecapi.appspot.com",
+  messagingSenderId: "36552924061",
+  appId: "1:36552924061:web:cd4e7809a74de6c6df6d74"
 };
 
 
-
-
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const storage = getStorage(app);
+// Initialize Firebase
 
-const storageRef = ref(storage);
+function adicionarDadosAdmin(userId, nomeProfessor, cpfProfessor, formacaoProfessor){
+const db = getDataBase();
+const reference = ref(db, 'admin/' + userId);
+
+set(reference, {
+
+  nome: nomeProfessor,
+  cpf: cpfProfessor,
+  formacao: formacaoProfessor,
+
+})
+}
+
+
+adicionarDadosAdmin("Dalva", "502.133.198-10", "Quimica");
