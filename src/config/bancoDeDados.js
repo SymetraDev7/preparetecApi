b6cdf5fe8ccfde4ed6bf53088307f1f6fe13daaa
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-08-19 02:54:16 -0300

    finish API 1.0

---
+++ src/config/bancoDeDados.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+
+// create the connection to database
+const conn = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  database: "preparetec",
+  password: "044825",
+  port: 3306,
+});
+
+module.exports = conn;
